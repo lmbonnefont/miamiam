08d7458bf3550296437285397fe59f04cd21c38b
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-09-09 17:28:32 +0200

    creation modele semaine

---
+++ app/models/semaine.rb
@@ -0,0 +1,3 @@
+class Semaine < ApplicationRecord
+  belongs_to :user
+end
---
+++ test/models/semaine_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SemaineTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
