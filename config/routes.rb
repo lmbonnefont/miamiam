c4def28801d42349da3941ade4460328e7328be0
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-09-09 19:06:01 +0200

    creation model controller et routing recette

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :semaines, only: [:new, :create, :show] do
-  end
+  resources :semaines, only: [:new, :create, :show]
+  resources :recettes, only: [:show, :index]
 end
