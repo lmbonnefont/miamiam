0bf4b82cbc96303c0f5447bd08b3e6eef24005ba
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2018-09-21 15:46:19 +0200

    la seed est un succès

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,31 +21,29 @@ recettes.each do |element|
   urls << element["href"]
 end
 
-# urls.each do |lien|
+urls.each do |lien|
 
-#   puts "ça tourne ..."
+  puts "ça tourne ..."
 
-#   url = "http://www.unjourunerecette.fr/#{lien}"
-#   html_file = open(url).read
-#   html_doc = Nokogiri::HTML(html_file)
+  url = "http://www.unjourunerecette.fr/#{lien}"
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
 
-#   titre = html_doc.search('.item').first.text
-#   titre = titre.strip
+  titre = html_doc.search('.item').first.text
+  titre = titre.strip
 
-#   instructions = html_doc.search('.instructions').first.text
-#   instructions = instructions.strip
+  instructions = html_doc.search('.instructions').first.text
+  instructions = instructions.strip
 
-#   nombre = html_doc.search('#entouringredients .courses').first.text
-#   nombre = nombre.strip.match(/\d/)[0].to_i
+  nombre = html_doc.search('#entouringredients .courses').first.text
+  nombre = nombre.strip.match(/\d/)[0].to_i
 
-#   Recette.create!(nom: titre, instruction: instructions, nb_personne: nombre)
+  recette = Recette.create!(nom: titre, instruction: instructions, nb_personne: nombre)
 
-#   puts "Et hop une recette dans la besace :)"
-# end
 
-url = "http://www.unjourunerecette.fr/recette-taboule-en-assiette-de-poivron"
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
+###############################################################################
+
+
 instructions = html_doc.search('#ingredients').text.strip.split(",")
 instructions.each do |ingredient|
   ingredient.gsub!("\n","")
@@ -72,25 +70,24 @@ p instructions
 instructions.each do |combinaison|
   if combinaison.match(/\d.\d/)
     quantité = combinaison.match(/\d.\d/).to_s
+    ingredient = combinaison[quantité.to_s.length + 1..-1]
   elsif combinaison.match(/\d/)
     quantité = combinaison.match(/\d/).to_s
+    ingredient = combinaison[quantité.to_s.length + 1..-1]
   else quantité = 1
+    ingredient = combinaison
   end
-  ingredient = combinaison[quantité.to_s.length + 1..-1]
-  p quantité.to_s.length + 1
-  p ingredient
-  p quantité
-#   if quantité == ""
-#     quantité = 1
-#     ingredient = combinaison
-#   else
-#     quantité = quantité[0].to_i
-#     ingredient = combinaison.match(/\s(.*)/)[0].strip
-#     if ingredient == nil
-#       ingredient = combinaison
-#     else
-#       ingredient = ingredient[0].strip
-#     end
-  # end
-   # p ingredient
+
+  dose = Dose.create!(quantite: quantité, ingredient:ingredient, recette: recette)
 end
+
+
+
+###############################################################################
+
+
+
+  puts "Et hop une recette dans la besace :)"
+end
+
+
