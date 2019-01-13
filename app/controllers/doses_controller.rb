e5de9086839857afc9778a28422f684f858e6c4c
LM Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2019-01-13 01:56:02 +0100

    possibilité d'enchainer la création des doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,9 +7,17 @@ class DosesController < ApplicationController
   def create
     dose = Dose.new(doses_params)
     recette = Recette.find(params[:recette_id])
+    recette.nb_doses_created += 1
+    recette.save
+
     dose.recette = recette
     dose.save!
-    redirect_to root_path
+
+    if recette.nb_doses_created == recette.nb_doses
+      redirect_to recette_path(recette)
+    else
+      redirect_to new_recette_dose_path(recette)
+    end
   end
 
   private
